2319032d2300664358deffd66f8cac8241d0b3ae
Jacob Olin <jolin00@stuy.edu>
2019-03-15 11:28:01 -0400

    Wrote the first part of mergesort's recursive method

--- Merge.java
+++ Merge.java
@@ -1,4 +1,12 @@
 public class Merge {
   public static void mergesort(int[] data) {
   }
+
+  public static void mergesort(int[] data,int lo,int hi) {
+    if (lo >= hi) {
+      return;
+    }
+    mergesort(data,lo,(lo+hi)/2);
+    mergesort(data,(lo+hi)/2+1,hi);
+  }
 }
