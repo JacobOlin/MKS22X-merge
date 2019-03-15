2ede6a2b9b2e583b651237088b29c381c94ec8aa
Jacob Olin <jolin00@stuy.edu>
2019-03-15 11:33:05 -0400

    Split the array into two arrays in merge

--- Merge.java
+++ Merge.java
@@ -12,6 +12,13 @@ public class Merge {
   }
 
   public static void merge(int[] data,int lo,int hi) {
-    
+    int[]data1 = new int[(lo+hi)/2 - lo + 1];
+    int[]data2 = new int[hi - (lo+hi)/2  + 1];
+    for (int i = 0;i < data1.length;i += 1) {
+      data1[i] = data[i + lo];
+    }
+    for (int i = 0;i < data2.length;i += 1) {
+      data2[data2.length - 1 - i] = data[hi - i];
+    }
   }
 }
